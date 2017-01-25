46400d35b24f101b538ef127097b2dbb7d5e93dc
whaag <eduardo.a.athayde@gmail.com>
2017-01-25 21:55:31 +0000

    improved angular

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -16,16 +16,27 @@
         {
         name: "ok gem",
         price: 1.58,
-        description: "a precious gem",
+        description: "dude...  I don't know about this one",
         canPurchase: true,
         soldOut: false
         },
         {
         name: "fake gem",
         price: 0.58,
-        description: "a precious gem",
+        description: "she'll never notice it",
         canPurchase: true,
         soldOut: false
         }
     ]
+
+    app.controller('PanelController', function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
 })();
