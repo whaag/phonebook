2620bcea293bca689c1a197a1c3e0e8ad6b89e55
whaag <eduardo.a.athayde@gmail.com>
2017-02-10 20:14:57 +0000

    archive old home.html and implement new

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,3 +1,26 @@
 (function() {
     var app = angular.module('phonebook', []);
+
+    app.controller('ListController', function() {
+        this.people = people;
+    });
+
+    var people = [
+        {
+            name: 'Eduardo Um',
+            phone: '555-2555'
+        },
+        {
+            name: 'Eduardo Dois',
+            phone: '555-2555'
+        },
+        {
+            name: 'Eduardo T43w',
+            phone: '555-2555'
+        },
+        {
+            name: 'Eduardo Quatro',
+            phone: '555-2555'
+        }
+    ];
 })();
